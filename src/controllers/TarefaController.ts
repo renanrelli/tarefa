99e5811a4f29db4196a1231c51e4f625992ed2ff
Renan Relli <renans.relli@gmail.com>
2023-09-04 17:04:04 -0300

    falta testar

--- src/controllers/TarefaController.ts
+++ src/controllers/TarefaController.ts
@@ -1,118 +1,130 @@
 import { Between } from "typeorm";
 import { Tarefa } from "../models/Tarefa";
-import promptSync from 'prompt-sync';
+import promptSync from "prompt-sync";
 import { Usuario } from "../models/Usuario";
 import { Categoria } from "../models/Categoria";
 const prompt = promptSync();
 
-export class TarefaController{
+export class TarefaController {
 
-  async create(descricao: string, prazo: string, categoriasIdcategoria: number,usuariosIdCriador: number, usuariosIdexecutor: number): Promise<Tarefa>{
+  async create(
+    descricao: string,
+    prazo: string,
+    categoriasIdcategoria: number,
+    usuariosIdCriador: number,
+    usuariosIdexecutor: number,
+    situacao: string
+  ): Promise<Tarefa | null> {
+    let usuarioExecutor: Usuario | null = await Usuario.findOneBy({
+      id: usuariosIdexecutor,
+    });
+    let categoria: Categoria | null = await Categoria.findOneBy({
+      id: categoriasIdcategoria,
+    });
 
-    let usuarioExecutor: Usuario | null = await Usuario.findOneBy({ id: usuariosIdexecutor });
-
-    if (! usuarioExecutor) {
-      throw new Error('ID do usuário executor não encontrado!');
+    if (!usuarioExecutor) {
+      console.log("ID do usuário executor não encontrado!");
+    } else if (!categoria) {
+      console.log("ID da categoria não encontrado");
+    } else {
+      return await Tarefa.create({
+        descricao: descricao,
+        prazo: prazo,
+        categorias_idcategoria: categoriasIdcategoria,
+        usuarios_idcriador: usuariosIdCriador,
+        usuarios_idexecutor: usuariosIdexecutor,
+        situacao: situacao,
+      }).save();
     }
-
-    let categoria: Categoria | null = await Categoria.findOneBy({id: categoriasIdcategoria})
-
-    if(!categoria){
-      throw new Error('ID da categoria não encontrado')
-    }
-
-     return await Tarefa.create({
-      descricao: descricao,
-      prazo: prazo,
-      categorias_idcategoria: categoriasIdcategoria,
-      usuarios_idcriador: usuariosIdCriador,
-      usuarios_idexecutor: usuariosIdexecutor,
-      situacao: 'A'
-    }).save();
-
+    return null;
   }
 
-  async list (): Promise<Tarefa[]>{
+  async list(): Promise<Tarefa[]> {
     return await Tarefa.find();
   }
 
-  async find(id: number): Promise<Tarefa|null>{
-    return await Tarefa.findOneBy({id})
+  async find(id: number): Promise<Tarefa | null> {
+    return await Tarefa.findOneBy({ id });
   }
 
-  async edit(tarefa: Tarefa, descricao: string, prazo: string, idCategoria: number, idExecutor: number): Promise<Tarefa>{
-    let categoria: Categoria | null = await Categoria.findOneBy({ id: idCategoria });
+  async edit(
+    tarefa: Tarefa,
+    descricao: string,
+    prazo: string,
+    idCategoria: number,
+    idExecutor: number,
+    situacao: string
+  ): Promise<Tarefa | null> {
+    let categoria: Categoria | null = await Categoria.findOneBy({
+      id: idCategoria,
+    });
     let executor: Usuario | null = await Usuario.findOneBy({ id: idExecutor });
 
-    if (! categoria) {
-      throw new Error('Categoria não encontrada!');
-    }
-    if(! executor){
-      throw new Error('Executor não encontrado')
-    }
-
-    tarefa.descricao = descricao;
-    tarefa.prazo = prazo;
-    tarefa.categorias_idcategoria = idCategoria;
-    tarefa.usuarios_idexecutor = idExecutor;
-    tarefa.save()
-    return tarefa;
-
-  };
-
-  async delete(){
-    let id = Number(prompt('Qual o ID?'));
-    let result = await Tarefa.delete({id: id})
-    if (result.affected && result.affected > 0){
-      console.log('Tarefa deletada com sucesso!')
-    } else (
-      console.log('ID da tarefa não encontrado!')
-    )
-    };
-
-    async listCategoria(){
-      let categoria: number = Number(prompt('Digite o número da categoria 1 - Trabalho, 2 - Estudo, 3 - Moradia, 4 - Startup: '));
-      let tarefasCategoria = await Tarefa.find({
-        where: {
-          categorias_idcategoria: categoria,
-        }
-    })
-    if(tarefasCategoria.length >= 1){
-      console.log(tarefasCategoria);
+    if (!categoria) {
+      console.log("Categoria não encontrada!");
+    } else if (!executor) {
+      console.log("Executor não encontrado");
     } else {
-      console.log('Nenhuma tarefa com o id foi encontrada');
+      tarefa.descricao = descricao;
+      tarefa.prazo = prazo;
+      tarefa.categorias_idcategoria = idCategoria;
+      tarefa.usuarios_idexecutor = idExecutor;
+      tarefa.situacao = situacao;
+      tarefa.save();
+      return tarefa;
     }
+    return null;
   }
 
-  async listTarefaSituacao(){
-    let situacao: string = prompt('Digite a situação da tarefa: A à fazer, F fazendo, C concluída ou I inativa: ').toUpperCase();
+  async delete(tarefa: Tarefa): Promise<Tarefa> {
+    tarefa.situacao = "I";
+    await tarefa.save();
+    return tarefa;
+  }
+
+  async listCategoria(idCategoria: number): Promise<Tarefa[]> {
     let tarefasCategoria = await Tarefa.find({
       where: {
-          situacao: situacao,
-      }
-  })
-  if(tarefasCategoria.length >= 1){
-    console.log(tarefasCategoria);
-  } else {
-    console.log('Nenhuma tarefa com a situação informada foi encontrada');
+        categorias_idcategoria: idCategoria,
+      },
+    });
+    return tarefasCategoria;
   }
-}
 
-async listTarefaPeriodo(){
-  let primeiraData: string = prompt('Informe a primeira data: aaaa-mm-dd: ')
-  let ultimaData: string = prompt('Informe a última data: aaaa-mm-dd: ')
-  let tarefasCategoria = await Tarefa.find({
-    where: {
-      prazo: Between(
-        primeiraData,
-        ultimaData
-      )
-    }
-})
-if(tarefasCategoria.length >= 1){
-  console.log(tarefasCategoria);
-} else {
-  console.log('Nenhuma tarefa com o prazo informado foi encontrado');
-}
-}
+  async listTarefaSituacao(situacao: string): Promise<Tarefa[]> {
+    let tarefasSituacao = await Tarefa.find({
+      where: {
+        situacao: situacao,
+      },
+    });
+    return tarefasSituacao;
+  }
+
+  async listTarefaPeriodo(dataInicio: string, dataFim: string) {
+    let tarefasPeriodo = await Tarefa.find({
+      where: {
+        prazo: Between(dataInicio, dataFim),
+        situacao: "A" || "C" || "F",
+      },
+    });
+    return tarefasPeriodo;
+  }
+
+  async listCriadas(idCriador: number): Promise<Tarefa[]> {
+    let tarefasCriadas = await Tarefa.find({
+      where: {
+        usuarios_idcriador: idCriador,
+      },
+    });
+    return tarefasCriadas;
+  }
+
+  async listExecutar(idExecutor: number): Promise<Tarefa[]> {
+    let tarefasExecutar = await Tarefa.find({
+      where: {
+        usuarios_idexecutor: idExecutor,
+      },
+    });
+    return tarefasExecutar;
+  }
 }
--- src/views/TarefaMenu.ts
+++ src/views/TarefaMenu.ts
@@ -2,78 +2,196 @@ import promptSync from "prompt-sync";
 const prompt = promptSync();
 import { Tarefa } from "../models/Tarefa";
 import { TarefaController } from "../controllers/TarefaController";
+import { Categoria } from "../models/Categoria";
 
-export class TarefaMenu{
-
+export class TarefaMenu {
   private usuarioLogin: number;
 
-  async idUsuarioLogado(id: number){
+  async idUsuarioLogado(id: number) {
     this.usuarioLogin = id;
   }
 
   public controller: TarefaController;
 
-  constructor(){
-    this.controller = new TarefaController()
+  constructor() {
+    this.controller = new TarefaController();
   }
 
-  public async show(){
-    console.log('9 - Criar tarefa');
-    console.log('10 - Listar todas tarefas');
-    console.log('11 - Editar tarefa');
-    console.log('12 - Inativar tarefa');
-    console.log('13 - Listar tarefas por categoria');
-    console.log('14 - Listar tarefas por situação');
-    console.log('15 - Listar tarefas por período');
-    console.log('16 - Listar tarefas criadas por você');
-    console.log('17 - Listar tarefas para serem feitas por você');
+  public async show() {
+    console.log("9 - Criar tarefa");
+    console.log("10 - Listar todas tarefas");
+    console.log("11 - Editar tarefa");
+    console.log("12 - Inativar tarefa");
+    console.log("13 - Listar tarefas por categoria");
+    console.log("14 - Listar tarefas por situação");
+    console.log("15 - Listar tarefas por período");
+    console.log("16 - Listar tarefas criadas por você");
+    console.log("17 - Listar tarefas para serem feitas por você");
   }
 
-  public async execute (input: string) {
+  public async execute(input: string) {
     switch (input) {
-      case '9':
+      case "9":
         await this.create();
         break;
-      case '10':
+      case "10":
         await this.list();
         break;
-      case '11':
+      case "11":
         await this.edit();
         break;
-      case '12':
-
-
+      case "12":
+        await this.delete();
+        break;
+      case "13":
+        await this.listCategoria();
+        break;
+      case "14":
+        await this.listSituacao();
+        break;
+      case "15":
+        await this.listPeriodo();
+        break;
+      case "16":
+        await this.listCriadas();
+        break;
+      case "17":
+        await this.listExecutar();
+        break;
     }
   }
 
-  private async create(){
+  private async showCategorias(){
+    let categorias = Categoria.find()
+    console.log(categorias);
+  }
+  private async create() {
     console.log(this.usuarioLogin);
 
-    let descricao = prompt('Digite a descrição da tarefa: ')
-    let prazo = prompt('Informe o prazo da tarefa (aaaa-mm-dd): ')
-    let idCategoria = Number(prompt('Digite o ID da categoria: '))
-    let idExecutor = Number(prompt('Digite o ID do usuário para executar a tarefa: '))
+    let descricao = prompt("Digite a descrição da tarefa: ");
+    let prazo = prompt("Informe o prazo da tarefa (aaaa-mm-dd): ");
+    let situacao = prompt(
+      "Informe a situação (A (à fazer), F (fazendo), C (concluída)): ");
+    console.log(`Categorias disponíveis: `);
+    this.listCategoria()
+    let idCategoria = Number(prompt("Digite o ID da categoria: "));
+    let idExecutor = Number(
+      prompt("Digite o ID do usuário para executar a tarefa: ")
+    );
 
-    let tarefa: Tarefa = await this.controller.create(descricao, prazo, idCategoria, this.usuarioLogin, idExecutor);
+    let tarefa: Tarefa | null = await this.controller.create(
+      descricao,
+      prazo,
+      idCategoria,
+      this.usuarioLogin,
+      idExecutor,
+      situacao
+    );
   }
 
-  private async list(): Promise<void>{
+  private async list(): Promise<void> {
     let tarefas: Tarefa[] = await this.controller.list();
     console.log(tarefas);
   }
 
-  private async edit(){
-    let id: number = Number(prompt('Qual é o ID?'))
-    let tarefa = await this.controller.find(id)
+  private async edit(): Promise<void> {
+    let id: number = Number(prompt("Qual é o ID?"));
+    let tarefa = await this.controller.find(id);
 
-    if(tarefa){
-    let descricao = prompt(`Descrição (${tarefa.descricao}): `);
-    let prazo = prompt(`Prazo (${tarefa.prazo}) aaaa-mm-dd: `)
-    let idCategoria = Number(prompt(`ID da categoria (${tarefa.categorias_idcategoria}): `))
-    let idExecutor = Number(prompt(`ID do executor (${tarefa.usuarios_idexecutor}): `))
-    await this.controller.edit(tarefa, descricao, prazo, idCategoria, idExecutor)
-
+    if (tarefa) {
+      let descricao = prompt(`Descrição (${tarefa.descricao}): `);
+      let prazo = prompt(`Prazo (${tarefa.prazo}) aaaa-mm-dd: `);
+      let situacao = prompt(
+        `Situação (${tarefa.situacao}) A ((à fazer), F (fazendo), C (concluída) ou I (inativa)) :`
+      );
+      let idCategoria = Number(
+        prompt(`ID da categoria (${tarefa.categorias_idcategoria}): `)
+      );
+      let idExecutor = Number(
+        prompt(`ID do executor (${tarefa.usuarios_idexecutor}): `)
+      );
+      await this.controller.edit(
+        tarefa,
+        descricao,
+        prazo,
+        idCategoria,
+        idExecutor,
+        situacao
+      );
     }
   }
 
+  private async delete(): Promise<void> {
+    let id: number = Number(prompt("Qual é o ID?"));
+    let tarefa = await this.controller.find(id);
+
+    if (tarefa) {
+      await this.controller.delete(tarefa);
+      console.log(`Tarefa ID ${tarefa.id} inativada com sucesso!`);
+    } else {
+      console.log(`Nenhuma tarefa com o ID ${id} foi encontrada!`);
+    }
+  }
+
+  private async listCategoria(): Promise<void> {
+    let categoria: number = Number(prompt("Qual é o ID da categoria?"));
+    let tarefas: Tarefa[] = await this.controller.listCategoria(categoria);
+    if (tarefas.length >= 1) {
+      console.log(tarefas);
+    } else {
+      console.log("Nenhuma tarefa com o id foi encontrada");
+    }
+  }
+
+  private async listSituacao(): Promise<void> {
+    let situacao: string = prompt(
+      "Digite a situação da tarefa: A à fazer, F fazendo, C concluída ou I inativa: "
+    ).toUpperCase();
+
+    let tarefasSituacao: Tarefa[] = await this.controller.listTarefaSituacao(
+      situacao
+    );
+
+    if (tarefasSituacao.length >= 1) {
+      console.log(tarefasSituacao);
+    } else {
+      console.log("Nenhuma tarefa com a situação informada foi encontrada");
+    }
+  }
+
+  private async listPeriodo() {
+    let primeiraData: string = prompt("Informe a primeira data: aaaa-mm-dd: ");
+    let ultimaData: string = prompt("Informe a última data: aaaa-mm-dd: ");
+
+    let tarefasPrazo: Tarefa[] = await this.controller.listTarefaPeriodo(
+      primeiraData,
+      ultimaData
+    );
+
+    if (tarefasPrazo.length >= 1) {
+      console.log(tarefasPrazo);
+    } else {
+      console.log("Nenhuma tarefa com o prazo informado foi encontrado");
+    }
+  }
+
+  private async listExecutar() {
+    let tarefasExecutar = await this.controller.listExecutar(this.usuarioLogin)
+
+    if (tarefasExecutar.length >= 1) {
+      console.log(tarefasExecutar);
+    } else {
+      console.log("Nenhuma tarefa para você executar");
+    }
+  }
+
+  private async listCriadas() {
+    let tarefasCriadas = await this.controller.listCriadas(this.usuarioLogin)
+
+    if (tarefasCriadas.length >= 1) {
+      console.log(tarefasCriadas);
+    } else {
+      console.log("Nenhuma tarefa criada por você");
+    }
+  }
 }
